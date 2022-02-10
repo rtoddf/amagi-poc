7adb329bd73871d304a660aa8f72c849cf668657
rtoddf <rtoddf@me.com>
2022-02-10 17:08:37 -0500

    forgot import

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -3,6 +3,7 @@ import video01 from './videos/video01.mp4';
 import video02 from './videos/video02.mp4';
 import video03 from './videos/video03.mp4';
 import video04 from './videos/video04.mp4';
+import video05 from './videos/video05.mp4';
 import thumbnail from './images/thumbnail.jpg';
 
 const Video = (props) => {
