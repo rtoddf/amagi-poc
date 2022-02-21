0e843da2947a759704f9980fc98b0b9635f991aa
Todd Fleeman <toddfleeman@CATL1LTFLEEMAN.local>
2022-02-21 14:57:25 -0500

    added images for full-screen

--- src/Components/data/weather-images.js
+++ src/Components/data/weather-images.js
@@ -2,6 +2,10 @@ const slideshow = {
   kiro: [
     {
       alt: 'alt tag',
+      url: 'http://mediaweb.kirotv.com/weather/Daypart-Tacoma.jpg',
+    },
+    {
+      alt: 'alt tag',
       url: 'http://mediaweb.kirotv.com/weather/metro-dce.jpg',
     },
     {
@@ -10,7 +14,27 @@ const slideshow = {
     },
     {
       alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/5-day-dce.jpg',
+    },
+    {
+      alt: 'alt tag',
       url: 'https://mediaweb.kirotv.com/weather/Bli-Radar-DCE.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/Daypart-Bellingham.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/Daypart-Everett.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/Daypart-Olympia.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/Daypart-Seattle.jpg',
     },
     {
       alt: 'alt tag',
@@ -28,8 +52,28 @@ const slideshow = {
       alt: 'alt tag',
       url: 'https://mediaweb.kirotv.com/weather/olympia-dce.jpg',
     },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/S-Coast-Radar-DCE.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/Tacoma-Radar-DCE.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/WW-RADAR-DCE.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.kirotv.com/weather/ww-sat-dce.jpg',
+    },
   ],
   wsb: [
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.wsbtv.com/weather/5day.jpg',
+    },
     {
       alt: 'alt tag',
       url: 'https://mediaweb.wsbtv.com/weather/highs.jpg',
@@ -40,15 +84,7 @@ const slideshow = {
     },
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.wsbtv.com/weather/highs.jpg',
-    },
-    {
-      alt: 'alt tag',
       url: 'https://mediaweb.wsbtv.com/weather/lows2.jpg',
-    },
-    {
-      alt: 'alt tag',
-      url: 'https://mediaweb.wsbtv.com/weather/highs.jpg',
     },
     {
       alt: 'alt tag',
@@ -57,6 +93,10 @@ const slideshow = {
     {
       alt: 'alt tag',
       url: 'https://mediaweb.wsbtv.com/weather/time2.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.wsbtv.com/weather/time3.jpg',
     },
     {
       alt: 'alt tag',
@@ -74,51 +114,87 @@ const slideshow = {
   wjax: [
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.actionnewsjax.com/weather/First_Alert_Radar.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+      url: 'https://mediaweb.actionnewsjax.com/weather/7_day.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/accuzone_forecast_coastal.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/accuzone_forecast_inland.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/adi_temps.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/beach.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/boating.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/Burntime.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/Duval_Zoom_Radar_2.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'hhttps://mediaweb.actionnewsjax.com/weather/First_Alert_Radar.jpg',
     },
     {
       alt: 'alt tag',
       url: 'https://mediaweb.actionnewsjax.com/weather/lightning.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
     },
     {
       alt: 'alt tag',
       url: 'https://mediaweb.actionnewsjax.com/weather/NE_FL_Radar.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/Right%20Now_Duval.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/Right%20Now.jpg',
     },
     {
       alt: 'alt tag',
       url: 'https://mediaweb.actionnewsjax.com/weather/SE_GA_Radar_2.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
     },
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.actionnewsjax.com/weather/First_Alert_Radar.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+      url: 'https://mediaweb.actionnewsjax.com/weather/sunrise_sunset.jpg',
     },
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.actionnewsjax.com/weather/lightning.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+      url: 'https://mediaweb.actionnewsjax.com/weather/travel_forecast.jpg',
     },
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.actionnewsjax.com/weather/NE_FL_Radar.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+      url: 'https://mediaweb.actionnewsjax.com/weather/warnings_2.jpg',
     },
     {
       alt: 'alt tag',
-      url: 'https://mediaweb.actionnewsjax.com/weather/SE_GA_Radar_2.jpg',
-      video:
-        'https://cmg.video-player.arcpublishing.com/prod/powaEmbed.html?org=cmg&env=prod&api=prod&uuid=179a3848-fb10-4441-b0f5-9531bc8ae184',
+      url: 'https://mediaweb.actionnewsjax.com/weather/Weekend_Forecast.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/zone_forecast_coastal.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/zone_forecast_inland.jpg',
+    },
+    {
+      alt: 'alt tag',
+      url: 'https://mediaweb.actionnewsjax.com/weather/zone_forecast_metro.jpg',
     },
   ],
 };
